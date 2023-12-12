7be58fb31b61d40b03761aac35613f1e2edc451f
Alessio Rugi <alessiorugi95@gmail.com>
2023-12-12 16:13:03 +0100

    add function to delete a task

--- js/script.js
+++ js/script.js
@@ -3,13 +3,10 @@ Rifare l’esercizio della to do list.
 Questa volta però ogni todo sarà un oggetto, formato da due proprietà:
 - text, una stringa che indica il testo del todo
 - done, un booleano (true/false) che indica se il todo è stato fatto oppure no.
-
-
 Stampare all’interno di una lista HTML un item per ogni todo.
 Se la proprietà done è uguale a true, visualizzare il testo del todo sbarrato.
 
-
-Visualizzare a fianco ad ogni item ha una “x”: cliccando su di essa, il todo viene rimosso dalla lista.
+Visualizzare a fianco ad ogni item una “x”: cliccando su di essa, il todo viene rimosso dalla lista.
 
 Predisporre un campo di input testuale e un pulsante “aggiungi”: cliccando sul pulsante, il testo digitato viene letto e utilizzato per creare un nuovo todo,
 che quindi viene aggiunto alla lista dei todo esistenti.
@@ -38,6 +35,11 @@ createApp({
                 done: false
                 }
             ], 
+        }
+    },
+    methods: {
+        delTask(i) {
+            this.todos.splice(i, 1);
         }
     },
 
